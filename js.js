83264d4f694a4d9b91d108182c5973afc28cfffc
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-09 22:29:04 +0600

    isNan

--- js.js
+++ js.js
@@ -1,7 +1,9 @@
-const num = 12;
+const num = [1,2,5]
 if(typeof num === 'number'){
     console.log("I am Number");
 }
 else{
     console.log("I am not a number");
 }
+
+console.log(typeof Array.isArray(num));
