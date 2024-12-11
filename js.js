6bb832b0b17bfa28c62c36c32bf3dfe0e28ae012
Asaduzzaman-Programmer <asaduzzamanprogrammer@gmail.com>
2024-12-11 17:12:19 +0600

    arrow

--- js.js
+++ js.js
@@ -6,3 +6,13 @@ else{
     console.log("I am not a number");
 }
 
+function sum(a, b){
+    return a + b;
+ }
+
+ const result = sum(90, 5)
+console.log(result);
+
+const res = (a, b) => a + b;
+const mul = res(90, 5);
+console.log(mul);
